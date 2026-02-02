9ad90cf96ca20caba041ea694f2eff0950120436
Rory Lawless <rorylawless@noreply.codeberg.org>
2026-02-01 23:52:28 -0500

    Update script to extract pbf data and create filtered version using bounding box for WMATA service area

--- code/02_pbf-extract/extract-osm-pbf.R
+++ code/02_pbf-extract/extract-osm-pbf.R
@@ -1,7 +1,9 @@
-library(tidyverse)
-library(sf)
+# Extract and filter pbf -------------------------------------------------
 
-conflicted::conflicts_prefer(dplyr::filter)
+library(readr)
+library(sf)
+library(osmextract)
+library(glue)
 
 # Load data
 shape_data <- read_csv("data/gtfs_feeds/wmata/shapes.txt")
@@ -14,3 +16,29 @@ shape_sf <- shape_data |>
 
 bounding_box <- shape_sf |>
 	st_bbox()
+
+# Get the pbf containing the bounding box
+# Set to not create the filter
+
+oe_get(
+	bounding_box,
+	provider = "geofabrik",
+	download_directory = "data/osm_extracts",
+	download_only = TRUE,
+	skip_vectortranslate = TRUE
+)
+
+# Run osmium extract shell command to extract the part of the pbf file that
+# corresponds with the WMATA bounding box
+
+bbox_arg <- glue_data(bounding_box, "--bbox {xmin},{ymin},{xmax},{ymax} ")
+
+system2(
+	"osmium",
+	args = c(
+		"extract data/osm_extracts/geofabrik_us-south-latest.osm.pbf",
+		bbox_arg,
+		"-o data/osm_extracts/wmata-extract-from-geofabrik_us-south-latest.osm.pbf",
+		"--overwrite"
+	)
+)
